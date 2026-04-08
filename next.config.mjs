db45ecadb65fb9d39005abbba294232e8e269ea9
Gustavo Silva <gustavocrsilva.ti@gmail.com>
2026-04-08 17:29:08 -0300

    feat: configure remote image patterns and remove unused dependencies and scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "myplants-api.systechdev.com.br",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
